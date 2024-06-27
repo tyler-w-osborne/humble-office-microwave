bc464bd665df94c8f0dcba99cb5467a4cde9aa08
Tyler Osborne <tosborne@djgrp.com>
2024-06-27 15:44:47 -0500

    changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,8 +3,8 @@ import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
-  { path: 'home', component: HomeComponent },
-  { path: 'about', component: AboutComponent },
+  { path: 'home', component: HomeComponent, title: 'home' },
+  { path: 'about', component: AboutComponent, title: 'about' },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: '**', redirectTo: '/home' },
 ];
