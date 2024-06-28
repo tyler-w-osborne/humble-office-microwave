3113669c626aa3257f295780a36c9d6e793490ea
Tyler Osborne <tosborne@djgrp.com>
2024-06-28 16:31:35 -0500

    changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 
+const title = (route: string): string => `Nuke Machine / ${route}`;
+
 export const routes: Routes = [
-  { path: 'home', component: HomeComponent, title: 'home' },
-  { path: 'about', component: AboutComponent, title: 'about' },
+  { path: 'home', component: HomeComponent, title: title('Home') },
+  { path: 'about', component: AboutComponent, title: title('About') },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: '**', redirectTo: '/home' },
 ];
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
+import { NgTemplateOutlet } from '@angular/common';
+import {
+  AfterViewInit,
+  Component,
+  OnInit,
+  TemplateRef,
+  ViewChild,
+} from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
 import { firstValueFrom } from 'rxjs';
@@ -7,16 +14,20 @@ import { MaterialsModule } from '../materials/materials.module';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [FormsModule, MaterialsModule],
+  imports: [FormsModule, MaterialsModule, NgTemplateOutlet],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, AfterViewInit {
   constructor(private _dialog: MatDialog) {
     this.People.Initialize();
   }
 
   ngOnInit(): void {}
+
+  ngAfterViewInit(): void {
+    //this.Timer.Launch();
+  }
 
   @ViewChild('AddPersonTemplate', { static: true })
   add_person_ref!: TemplateRef<any>;
@@ -71,26 +82,89 @@ export class HomeComponent implements OnInit {
       Person: <Person>{ Name: null, Time: null },
       Execute: (person: Person) => {
         this.People.Selected.Person = person;
-        this.Keyboard.Set(person.Time);
+        this.Timer.Set(person.Time);
       },
     },
   };
 
-  Keyboard = {
-    Hours: <number>0,
+  @ViewChild('TimerTemplate', { static: false }) timer_ref: TemplateRef<any>;
+  Timer = {
+    STR: <[number, number, number, number]>[null, null, null, null],
+    //Hours: <number>0,
     Minutes: <number>0,
     Seconds: <number>0,
     Set: (total_seconds: number) => {
-      const hours = total_seconds / 3600;
-      const minutes = (hours - Math.floor(hours)) * 60;
+      //const hours = total_seconds / 3600;
+      const minutes = total_seconds / 60;
       const seconds = (minutes - Math.floor(minutes)) * 60;
-      console.log('Hours:', Math.floor(hours));
+      //console.log('Hours:', Math.floor(hours));
       console.log('Minutes:', Math.floor(minutes));
       console.log('Seconds:', Math.round(seconds));
       //const minutes
-      this.Keyboard.Hours = Math.floor(hours);
-      this.Keyboard.Minutes = Math.floor(minutes);
-      this.Keyboard.Seconds = Math.round(seconds);
+      //this.Timer.Hours = Math.floor(hours);
+      this.Timer.Minutes = Math.floor(minutes);
+      this.Timer.Seconds = Math.round(seconds);
+      // const timer_str = [];
+      // const str_minutes = `${Math.floor(minutes)}`;
+      // if (str_minutes.length === 2) {
+      //   timer_str.push(Number(str_minutes[0]), Number(str_minutes[1]));
+      // } else {
+      //   timer_str.push(null, Math.floor(minutes));
+      // }
+      //this.Timer.STR = [null, Math.floor(minutes), null, Math.round(seconds)];
+    },
+    Launch: (person: Person) => {
+      this.Timer.Set(person.Time);
+      console.log(this.Timer.STR);
+      this._dialog.open(this.timer_ref);
+    },
+    AddTime: (digit: number) => {
+      const reverse_index = this.Timer.STR.findIndex(
+        (str_digit) => !!!str_digit
+      );
+      if (reverse_index === -1) {
+        console.log(this.Timer.STR);
+        return;
+      }
+      this.Timer.STR.shift();
+      this.Timer.STR.push(digit);
+    },
+    ClearTime: () => {
+      this.Timer.STR = [null, null, null, null];
+    },
+    DigitSplit: (
+      minutes: number,
+      seconds: number
+    ): [number, number, number, number] => {
+      const digit_array = <[number, number, number, number]>[
+        null,
+        null,
+        null,
+        null,
+      ];
+      const str_minutes = `${minutes}`;
+      if (str_minutes.length >= 2) {
+        digit_array.splice(
+          0,
+          2,
+          Number(str_minutes[0]),
+          Number(str_minutes[1])
+        );
+      } else {
+        digit_array.splice(1, 1, minutes);
+      }
+      const str_seconds = `${seconds}`;
+      if (str_seconds.length >= 2) {
+        digit_array.splice(
+          2,
+          2,
+          Number(str_seconds[0]),
+          Number(str_seconds[1])
+        );
+      } else {
+        digit_array.splice(3, seconds);
+      }
+      return digit_array;
     },
   };
 }
